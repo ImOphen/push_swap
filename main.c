7dae253e1a1af22fb6a17bad92f36b7dee291289
Ayman Touhami <atouhami@e1r1p3.1337.ma>
2021-12-17 14:05:17 +0100

    updated algorithm

--- main.c
+++ main.c
@@ -60,6 +60,68 @@ int does_have_smaller(int *stack, int elements_nb, int half)
 
 }
 
+int find_closest_smaller(int *stack, int elements_nb, int half)
+{
+	int i;
+	int closest;
+	int rev_closest;
+	int el_cpy;
+
+	// el_cpy = elements_nb;
+	rev_closest = 1;
+	i = 0;
+	while(i < elements_nb)
+		{
+			if (stack[i] < half)
+			{
+				closest = i;
+				break;
+			}
+			i++;
+		}
+	while(elements_nb--)
+		{
+			if (stack[elements_nb] < half)
+				break;
+			rev_closest++;
+
+		}
+	if (closest < rev_closest)
+		return(1);
+	else
+		return 0;
+}
+
+int find_closest_biggest(int *stack, int elements_nb, int biggest)
+{
+	int i;
+	int closest;
+	int rev_closest = 1;
+
+	// el_cpy = elements_nb;
+	i = 0;
+	while(i < elements_nb)
+		{
+			if (stack[i] == biggest)
+			{
+				closest = i;
+				break;
+			}
+			i++;
+		}
+	while(elements_nb--)
+		{
+			if (stack[elements_nb] == biggest)
+				break;
+			rev_closest++;
+
+		}
+	if (closest < rev_closest)
+		return(1);
+	else
+		return 0;
+}
+
 void put_bigger_to_b(int *stack, int *stack_b, int *elements_nb, int *b_elements_nb)
 {
 	int half;
@@ -72,20 +134,16 @@ void put_bigger_to_b(int *stack, int *stack_b, int *elements_nb, int *b_elements
 				if (stack[0] < half)
 					pb(stack, stack_b, elements_nb, b_elements_nb);
 				else
-					ra(stack, *elements_nb);
+				{
+			//		if (find_closest_smaller(stack, *elements_nb, half))
+						ra(stack, *elements_nb);
+				//	else
+			//			rra(stack, *elements_nb);
+				}
 			}
 		}
-	// while(*elements_nb >= 2)
-	// 	{
-	// 		half = sort_array_half(stack, *elements_nb);
-	// 		while((does_have_smaller(stack, *elements_nb, half)))
-	// 		{
-	// 			if (stack[0] < half)
-	// 				pb(stack, stack_b, elements_nb, b_elements_nb);
-	// 			else
-	// 				ra(stack, *elements_nb);
-	// 		}
-	// 	}
+	if (stack[0] > stack[1])
+		sa(stack);
 }
 
 void sort_to_bigger(int *stack_b,int b_elements_nb)
@@ -101,14 +159,7 @@ void sort_to_bigger(int *stack_b,int b_elements_nb)
 			biggest = stack_b[i];
 		i++;
 	}
-	i = 0;
-	while (i < b_elements_nb)
-		{
-			if (stack_b[i] == biggest)
-				break;
-			i++;
-		}
-	if (i < b_elements_nb / 2)
+	if (find_closest_biggest(stack_b, b_elements_nb, biggest))
 		while(stack_b[0] != biggest)
 			rb(stack_b, b_elements_nb);
 	else
@@ -129,13 +180,8 @@ void push_back_to_a(int *stack, int *stack_b, int *elements_nb, int *b_elements_
 void sort(int *stack, int *stack_b, int *elements_nb, int *b_elements_nb)
 {
 
-	if (*elements_nb <= 2)
-	{
-		if (*elements_nb == 1)
+	if (*elements_nb < 2)
 			return ;
-		if (stack[0] > stack[1])
-			sa(stack);
-	}
 	else if (!(is_sorted(stack, *elements_nb)))
 	{
 		put_bigger_to_b(stack, stack_b, elements_nb, b_elements_nb);
@@ -160,7 +206,7 @@ int main(int argc, char *argv[])
 	stack = handle_arguments(argc, argv, &elements_nb);
 	stack_b = malloc(elements_nb * sizeof(int));
 	sort(stack, stack_b, &elements_nb, &b_elements_nb);
-	//testing(stack, stack_b, &elements_nb, &b_elements_nb);
+	testing(stack, stack_b, &elements_nb, &b_elements_nb);
 	free(stack);
 	free(stack_b);
 	return (0);
